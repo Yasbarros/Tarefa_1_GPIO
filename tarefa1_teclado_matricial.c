be49d94452861e129317725df70bdd9776c06498
Yasmim <barrosyasmim13@gmail.com>
2025-01-17 21:58:34 -0300

    Função Principal

---
+++ tarefa1_teclado_matricial.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+
+
+int main()
+{
+    stdio_init_all();
+    init_gpio();
+
+    char last_key = 0; // Variável para armazenar a última tecla pressionada
+
+
+    while (1) {
+        
+    }
+}
