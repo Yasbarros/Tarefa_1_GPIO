25da91eac47a8d0ec80fb445e3f657ba91b74175
Luan Souza <luansouzameira@gmail.com>
2025-01-17 22:23:04 -0300

    mapeamento das teclas

--- tarefa1_teclado_matricial.c
+++ tarefa1_teclado_matricial.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
-#include "pico/stdlib.h"
-#include "hardware/gpio.h"
+#include <pico/stdlib.h>
+#include <hardware/gpio.h>
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Mapeamento das teclas do teclado matricial
+const char keys[ROWS][COLS] = {
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'}
+};
 
 int main()
 {
