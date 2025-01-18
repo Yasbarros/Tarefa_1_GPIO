2f708e54911a72998c5b34d89379b9ad0ab2f637
Victor Gabriel <vic.gabriel7@gmail.com>
2025-01-17 22:35:51 -0300

    configuração das linhas do teclado como saída

--- tarefa1_teclado_matricial.c
+++ tarefa1_teclado_matricial.c
@@ -2,6 +2,14 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
+void init_gpio() {
+    //Configuração das linhas do teclado como saídas
+    for (int i = 0; i < ROWS; i++) {
+        gpio_init(row_pins[i]);
+        gpio_set_dir(row_pins[i], GPIO_OUT);
+        gpio_put(row_pins[i], 1); // Inicializa em estado alto
+    }
+}
 
 int main()
 {
