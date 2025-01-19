60293ef0face71f211c3fa6683756b4732f30014
Yasmim <barrosyasmim13@gmail.com>
2025-01-19 16:34:04 -0300

    Ajustes

--- tarefa1_teclado_matricial.c
+++ tarefa1_teclado_matricial.c
@@ -73,29 +73,42 @@ int main()
 
     while (1) {
         char key = scan_keypad();
-        switch (key) {
-            case 'A':
-                gpio_put(LED3, 1); // Acende LED3
-                break;
-            case 'B':
-                gpio_put(LED2, 1); // Acende LED2
-                break;
-            case 'C':
-                gpio_put(LED1, 1); // Acende LED1
-                break;
-            case 'D':
-                // Acende todos os LEDs
-                gpio_put(LED1, 1);
-                gpio_put(LED2, 1);
-                gpio_put(LED3, 1);
-                break;
-            case '*':
-                gpio_put(BUZZER_PIN, 1); // Liga buzzer
-                break;
-            default:
-                break;
-        }
-        last_key = key;
-    }
 
+        if (key && key != last_key) { // Apenas processa nova tecla
+            printf("Tecla pressionada: %c\n", key);
+
+            switch (key) {
+                case 'A':
+                    gpio_put(LED3, 1); // Acende LED3
+                    break;
+                case 'B':
+                    gpio_put(LED2, 1); // Acende LED2
+                    break;
+                case 'C':
+                    gpio_put(LED1, 1); // Acende LED1
+                    break;
+                case 'D':
+                    // Acende todos os LEDs
+                    gpio_put(LED1, 1);
+                    gpio_put(LED2, 1);
+                    gpio_put(LED3, 1);
+                    break;
+                case '*':
+                    gpio_put(BUZZER_PIN, 1); // Liga buzzer
+                    break;
+                default:
+                    break;
+            }
+            last_key = key;
+        }
+        if (!key && last_key) { // Detecta liberação de tecla
+            // Desliga LEDs e buzzer quando a tecla é liberada
+            gpio_put(LED1, 0);
+            gpio_put(LED2, 0);
+            gpio_put(LED3, 0);
+            gpio_put(BUZZER_PIN, 0);
+
+            last_key = 0; // Reseta a última tecla
+        }
+    }
 }
